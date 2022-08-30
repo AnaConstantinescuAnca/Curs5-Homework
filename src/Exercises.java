b8a90a1d100742d3fa5c81311e319220a58b33f0
AnaConstantinescuAnca <ana.constantinescu@gmail.com>
2022-08-30 14:59:53 +0300

    Third commit - completare exercitiu

--- src/Exercises.java
+++ src/Exercises.java
@@ -13,7 +13,10 @@ public class Exercises {
 
         System.out.println("Ex.1: Suma numerelor intregi din sir este: " + sumNumereIntregi(sirNumereIntregi));
         System.out.println("Ex.2: Numarul de elemente impare din sir este: " + nrElementeImpare(sirNumereIntregi));
-        System.out.println(("Ex.3: Numerele mai mari decat nr primit sunt: " + Arrays.toString(nrNumereMaiMari(sirNumereIntregi, altnumarInterg))));
+
+        System.out.println(("Ex.3: Rezolvarea mea; Numerele mai mari decat nr primit sunt: " + Arrays.toString(nrNumereMaiMari(sirNumereIntregi, altnumarInterg))));
+        System.out.println("Ex.3: rezolvarea din clasa" + Arrays.toString(intNrMaiMari(sirNumereIntregi, altnumarInterg)));
+
         System.out.println("Ex4.: ");
         targetDonatii(target);
         System.out.println("Ex.5: ");
@@ -21,8 +24,6 @@ public class Exercises {
         System.out.println("Ex.6: ");
         printarePeMaiMulteLinii(fraza);
     }
-
-
 
 
     //1.scrieti o functie care primeste un sir de numere intregi si returneaza suma lor
@@ -49,6 +50,24 @@ public class Exercises {
     //3) scrieti o functie care primeste un sir de numere intregi si un alt numar intreg.
     // Returnati toate numerele mai mari decat numarul primit
 
+    public static int[] intNrMaiMari(int[] sirNumereIntregi, int altNumarIntreg) {
+        int[] rezultat = new int[sirNumereIntregi.length];
+
+        int counter = 0;
+
+        for (int n : sirNumereIntregi) {
+            if (n > altNumarIntreg) {
+                rezultat[counter++] = n;
+            }
+        }
+
+        int[] rezultatActual = new int[counter];
+        for (int i = 0; i < counter; i++) {
+            rezultatActual[i] = rezultat[i];
+        }
+        return rezultatActual;
+    }
+
     public static String[] nrNumereMaiMari(int[] sirNumereIntregi, int altnumarInterg) {
         String rezultat = "";
 
@@ -68,8 +87,8 @@ public class Exercises {
         //    rezultat = rezultat.substring(0, rezultat.length() - 1);
         //}
 
-       // String[] rezultatArray = rezultat.split(",");
-       // return rezultatArray;
+        // String[] rezultatArray = rezultat.split(",");
+        // return rezultatArray;
 
     }
 
@@ -79,7 +98,7 @@ public class Exercises {
     // Cand ajungem la suma dorita afisam un mesaj de succes.
     public static void targetDonatii(int target) {
         Random random = new Random();
-        int sumaDonata ;//= 0;
+        int sumaDonata;//= 0;
         int sumaObtinuta = 0;
 
         do {
@@ -114,11 +133,11 @@ public class Exercises {
             if (sumaObtinuta == target) {
                 System.out.println("Targetul a fost atins! Campania de donatii continua!");
             } else if (sumaObtinuta > target) {
-                System.out.println("Targetul a fost depasit cu: " + (sumaObtinuta-target) + "! Campania continua!");
+                System.out.println("Targetul a fost depasit cu: " + (sumaObtinuta - target) + "! Campania continua!");
             }
         } while (numarDonatii <= nrMaxDonatii);
         if (sumaObtinuta < target) {
-            System.out.println("Campania s-a incheiat! Pentru a atinge targetul mai era nevoie de: " + (target - sumaObtinuta) );
+            System.out.println("Campania s-a incheiat! Pentru a atinge targetul mai era nevoie de: " + (target - sumaObtinuta));
         } else {
             System.out.println("Campania s-a incheiat! Targetul a fost atins!");
         }
@@ -129,7 +148,7 @@ public class Exercises {
     // Printati fiecare propozitie pe o linie noua
     public static void printarePeMaiMulteLinii(String fraza) {
         String[] propozitii = fraza.split("[.]");
-        for ( int i = 0; i< propozitii.length;i++){
+        for (int i = 0; i < propozitii.length; i++) {
             System.out.println(propozitii[i].trim().concat("."));
         }
     }
